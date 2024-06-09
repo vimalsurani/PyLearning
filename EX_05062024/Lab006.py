5321db2c15767a037e67c8e9ae2bcf126d2f40d5
ViMS <vimal.surani@gmail.com>
2024-06-09 21:21:28 +0530

    Adding Lab006 File

---
+++ EX_05062024/Lab006.py
@@ -0,0 +1,9 @@
+# Program to find the max between two numbers
+
+print(max(10, 25))
+print(max(10, 25, 45))
+print(max(10, 25, 45, -1, 105, 87.54))
+
+# TypeError: '>' not supported between instances of 'str' and 'int'
+
+# print(max(10, 25, 45, -1, 105, 87.54,"Vims"))
