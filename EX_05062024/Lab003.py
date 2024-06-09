7d97dbf9cea8d97bef4eaa4dcd4749735b08a4d9
ViMS <vimal.surani@gmail.com>
2024-06-09 21:00:04 +0530

    Adding Lab003 File

---
+++ EX_05062024/Lab003.py
@@ -0,0 +1,15 @@
+# print()
+# self - Concept in OOPs which points to itself
+# *args - Unlimited no. of arguments * - string, int, float, boolean...
+# sep =' ' - How you want to separate the arguments.
+# end = '\n' - in the end what want to do
+# file = none - File IO
+
+print("Hello", "World", 123, True, 3.14)
+print("Hello", "World", 456, True, 4.14, sep="-")
+print("Hello", "World", 789, True, 5.14, sep="#")
+print("Hello", "World", 101, True, 6.14, sep="  ")
+print("Hello", "World", 456, True, 4.14, sep=":")
+
+print("Hello,My Name is Vimal", "Patel")
+print("Hello,My Name is ", " Vimal Patel", sep=":")
