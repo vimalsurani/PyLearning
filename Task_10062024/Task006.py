20c3f2857f540ee9ddc11b3e5f642bdf8cb26708
Vimal <vimal.surani@gmail.com>
2024-06-30 19:01:17 +0530

    String

---
+++ Task_10062024/Task006.py
@@ -0,0 +1,35 @@
+name = "VIMAL PATEL"
+
+# string.index(value, start, end)
+
+# print(name.count(name))
+#
+# print(name.upper())
+#
+# print(name.lower())
+
+# print(name.split("#"))
+
+# print(name.swapcase())
+
+# print(name.index("A",5,5))  # ValueError: substring not found
+# print(name.find("A",5,5))   # -1 not found
+
+# The index() method is almost the same as the find() method,
+# the only difference is that the find() method returns -1 if the value is not found.
+#
+# print("my name is {:,} ".format(45555555))
+#
+# print(name.strip())
+#
+# print(name.capitalize())
+#
+# print(name.casefold())
+#
+# print(name.endswith("V"))
+
+name1 = "   Vimal Patel      "
+
+print(name1.strip())  # Remove spaces at the beginning and at the end of the string:
+
+
